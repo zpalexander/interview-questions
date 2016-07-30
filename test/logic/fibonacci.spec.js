e8e7c72acb7859c06fe9bf8296b5d19e82bfea8e
Zach Alexander <zpalexander@gmail.com>
2016-07-30 19:26:48 -0400

    Couple more test harnesses

---
+++ test/logic/fibonacci.spec.js
@@ -0,0 +1,36 @@
+/**
+ * fibonacci.spec.js
+ *
+ * Test harness for fibonacci functions
+ */
+/* Dependencies */
+import expect from 'expect';
+import { fibonacciIterative, fibonacciRecursive } from '../../solutions/logic/fibonacci';
+
+/* Test Cases */
+describe('Logic: Fibonacci', () => {
+  const input1 = 0;
+  const expectedRes1 = 0;
+  const input2 = 4;
+  const expectedRes2 = 3;
+  const input3 = 10;
+  const expectedRes3 = 55;
+
+  it('should handle calculating fibonacci iteratively', () => {
+    // Case 1
+    expect(fibonacciIterative(input1)).toEqual(expectedRes1);
+    // Case 2
+    expect(fibonacciIterative(input2)).toEqual(expectedRes2);
+    // Case 3
+    expect(fibonacciIterative(input3)).toEqual(expectedRes3);
+  });
+
+  it('should handle calculating fibonacci recursively', () => {
+    // Case 1
+    expect(fibonacciRecursive(input1)).toEqual(expectedRes1);
+    // Case 2
+    expect(fibonacciRecursive(input2)).toEqual(expectedRes2);
+    // Case 3
+    expect(fibonacciRecursive(input3)).toEqual(expectedRes3);
+  });
+});
---
+++ test/logic/pascals-triangle.spec.js
@@ -0,0 +1,27 @@
+/**
+ * pascals-triangle.spec.js
+ *
+ * Test harness for Pascal's Triangle problem
+ */
+/* Dependencies */
+import expect from 'expect';
+import pascalsTriangle from '../../solutions/logic/pascals-triangle';
+
+/* Test Cases */
+describe('Logic: Pascal\'s Triangle', () => {
+  it('should return the first row of pascal\'s triangle', () => {
+    expect(pascalsTriangle(1)).toEqual([1]);
+  });
+
+  it('should return the second row of pascal\'s triangle', () => {
+    expect(pascalsTriangle(2)).toEqual([1, 1]);
+  });
+
+  it('should return the third row of pascal\'s triangle', () => {
+    expect(pascalsTriangle(3)).toEqual([1, 2, 1]);
+  });
+
+  it('should return the seventh row of pascal\'s triangle', () => {
+    expect(pascalsTriangle(7)).toEqual([1, 6, 15, 20, 15, 6, 1]);
+  });
+});
