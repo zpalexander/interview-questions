5ff5e4b4543f74d5f3f122266be42e25ab07deda
Zach Alexander <zpa@amazon.com>
2022-02-12 16:10:04 -0800

    Add another frankenzombie test case

--- test/logic/frankenzombie.spec.js
+++ test/logic/frankenzombie.spec.js
@@ -7,11 +7,18 @@ import { totalTime } from '../../solutions/logic/frankenzombie';
 
 /* Test Cases */
 describe('Logic: Frankenzombie', () => {
-  it('should handle a simple list', () => {
+  it('should handle a simple overlapping list', () => {
     const sampleInput = [[1, 3], [2, 6]];
     const expectedResult = 5;
     const actualResult = totalTime(sampleInput);
     expect(actualResult).toEqual(expectedResult);
+  });
+
+  it('should handle a list where cards do not overlap', () => {
+    const sampleInput = [[1, 3], [5, 6], [20, 24]];
+    const expectedResult = 7;
+    const actualResult = totalTime(sampleInput);
+    expect(actualResult).toEqual(expectedResult)
   });
 
   it('should handle a more complicated list', () => {
